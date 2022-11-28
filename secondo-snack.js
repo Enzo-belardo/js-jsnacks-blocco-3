c989e479cb1b05a0be311eb72b92c8fb9b93dd6d
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-28 18:29:12 +0100

    ulteriori modifiche programma

--- secondo-snack.js
+++ secondo-snack.js
@@ -1,13 +1,17 @@
-// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array.
+// Continua a chiedere i numeri all'utente e a inserirli nell'array 
+// fino a quando la somma degli elementi è minore di 50.
 
 let number = [];
 somma= 0;
 
-let numberUser= prompt('inserisci numero meno di 50'); ;
-
-while (somma <= 50) {
-    numberUser = prompt('inserisci numero meno di 50'); I
+while (somma < 50) {
+    let numberUser= parseInt(prompt('inserisci numero meno di 50')); 
     somma += numberUser;
-    number.push(numberUser);  
+    number.push(numberUser); 
+    if( somma > 50) {
+        console.log('Hai superato il valore massimo')
+    }
 }
+
 console.log(number);
