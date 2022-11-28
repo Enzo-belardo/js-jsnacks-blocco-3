2f8335a3d86b2ad683e2d82447a96a334a3e39bf
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-28 16:36:49 +0100

    snack uno

---
+++ script.js
@@ -0,0 +1,10 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+
+let number = 7;
+
+let numbers = prompt('inserisci numero');
+while (numbers != number) {
+    numbers = prompt('inserisci numero');
+}
+
